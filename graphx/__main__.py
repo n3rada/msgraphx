ca6627076846eada6d431c1ee536761f2d4fc244
n3rada <72791564+n3rada@users.noreply.github.com>
2025-07-11 10:54:00 +0200

    Add __main__.py

---
+++ graphx/__main__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Standard library imports
+import sys
+
+# External library imports
+from loguru import logger
+
+# Local library imports
+from graphx import console
+
+
+if __name__ == "__main__":
+    try:
+        sys.exit(console.run())
+    except KeyboardInterrupt:
+        logger.debug("🛑 User interrupted the process.")
+        sys.exit(0)
